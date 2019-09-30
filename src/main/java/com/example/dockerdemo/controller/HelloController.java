e966bcbea9cdd45b4b3e463fbdf0fa287bc79548
wenhaow <wenhaow@foxmail.com>
2019-09-30 16:41:11 +0800

    commit first
    Signed-off-by: wenhaow <wenhaow@foxmail.com>

---
+++ src/main/java/com/example/dockerdemo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
